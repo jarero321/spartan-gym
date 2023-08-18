598f4d1c29b41cf48e16b41c08b76d1e151b9e19
TajwarSaiyeed <rjabid36@gmail.com>
2023-08-18 21:42:35 +0600

    update

--- app/(root)/user/exercise/page.tsx
+++ app/(root)/user/exercise/page.tsx
@@ -9,6 +9,7 @@ import {
   CssBaseline,
   Grid,
   Paper,
+  Skeleton,
   Table,
   TableBody,
   TableCell,
@@ -42,9 +43,67 @@ const MyExercisePage = () => {
   }
 
   if (isLoading) {
-    return <Loading />;
-  }
+    return (
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="lg">
+          <CssBaseline />
+          <Box>
+          <Skeleton animation="wave" variant="text" sx={{ fontSize: '3rem' }} />
 
+          </Box>
+          <Box
+            sx={{
+              my: 3,
+            }}
+          >
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <Box
+                  p={2}
+                  sx={{
+                    border: "1px solid #333",
+                    borderRadius: "7px",
+                  }}
+                >
+                  <Typography component="h2" variant="h4">
+                    <Skeleton animation="wave" variant="text" sx={{ fontSize: "2rem" }} />
+                  </Typography>
+                  <Typography component="h2" variant="h4">
+                    <Skeleton animation="wave" variant="text" sx={{ fontSize: "2rem" }} />
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Box
+                  p={2}
+                  sx={{
+                    border: "1px solid #333",
+                    borderRadius: "7px",
+                  }}
+                >
+                  <Typography component="h2" variant="h4">
+                    <Skeleton animation="wave" variant="text" sx={{ fontSize: "2rem" }} />
+                  </Typography>
+                  <Typography component="h2" variant="h4">
+                    <Skeleton animation="wave" variant="text" sx={{ fontSize: "2rem" }} />
+                  </Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+          <Box>
+          <Skeleton animation="wave" variant="rounded" sx={{
+            width: "100%",
+            height: {
+              xs: 200,
+              sm: 300
+            }
+          }}/>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    );
+  }
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -55,42 +114,80 @@ const MyExercisePage = () => {
             My Exercise
           </Typography>
         </Box>
-        <Box sx={{
-          mt: 3
-        }}>
-        <Grid container>
-          <Grid item xs={12}>
-            <TableContainer component={Paper}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Name</TableCell>
-                    <TableCell>Sets</TableCell>
-                    <TableCell>Steps</TableCell>
-                    <TableCell>Kg</TableCell>
-                    <TableCell>Rest Time</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody
-                  sx={{
-                    textAlign: "center",
-                  }}
-                >
-                  {data.exercises.length > 0 &&
-                    data?.exercises?.map((exercise: CustomizedExercise) => (
-                      <TableRow key={exercise?.id}>
-                        <TableCell>{exercise?.exerciseName}</TableCell>
-                        <TableCell>{exercise?.sets}</TableCell>
-                        <TableCell>{exercise?.steps}</TableCell>
-                        <TableCell>{exercise?.kg}</TableCell>
-                        <TableCell>{exercise?.rest}</TableCell>
-                      </TableRow>
-                    ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+        <Box
+          sx={{
+            my: 3,
+          }}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <Box
+                p={2}
+                sx={{
+                  border: "1px solid #333",
+                  borderRadius: "7px",
+                }}
+              >
+                <Typography component="h2" variant="h4">
+                  From
+                </Typography>
+                <Typography component="h2" variant="h4">
+                  {new Date(data.fromDate).toISOString().slice(0, 10)}
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Box
+                p={2}
+                sx={{
+                  border: "1px solid #333",
+                  borderRadius: "7px",
+                }}
+              >
+                <Typography component="h2" variant="h4">
+                  To
+                </Typography>
+                <Typography component="h2" variant="h4">
+                  {new Date(data.toDate).toISOString().slice(0, 10)}
+                </Typography>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
+        </Box>
+        <Box>
+          <Grid container>
+            <Grid item xs={12}>
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Name</TableCell>
+                      <TableCell>Sets</TableCell>
+                      <TableCell>Steps</TableCell>
+                      <TableCell>Kg</TableCell>
+                      <TableCell>Rest Time</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody
+                    sx={{
+                      textAlign: "center",
+                    }}
+                  >
+                    {data.exercises.length > 0 &&
+                      data?.exercises?.map((exercise: CustomizedExercise) => (
+                        <TableRow key={exercise?.id}>
+                          <TableCell>{exercise?.exerciseName}</TableCell>
+                          <TableCell>{exercise?.sets}</TableCell>
+                          <TableCell>{exercise?.steps}</TableCell>
+                          <TableCell>{exercise?.kg}</TableCell>
+                          <TableCell>{exercise?.rest}</TableCell>
+                        </TableRow>
+                      ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
+          </Grid>
         </Box>
       </Container>
     </ThemeProvider>
